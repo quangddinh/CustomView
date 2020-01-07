dc1aeb534ee333f0e7e7eee584f99f9e4370c92d
Dinh Quang <quangddinh@gmail.com>
2020-01-07 18:55:34 +0700

    init

---
+++ app/src/main/java/com/example/customview/EditTextRedPointWidget.java
@@ -0,0 +1,22 @@
+package com.example.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class EditTextRedPointWidget extends RelativeLayout {
+    public EditTextRedPointWidget(Context context) {
+        super(context);
+    }
+
+
+
+    public EditTextRedPointWidget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EditTextRedPointWidget(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
+// run time
